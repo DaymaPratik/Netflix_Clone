62ebb87bee4a2b5906ede0de5d5b47f2a5044b1d
Pratik Dayma <pratikdayma45@gamil.com>
2024-08-03 11:53:29 +0530

    commit 2

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: 'https://netflix-clone-henna-pi.vercel.app/',
+    origin: 'https://netflix-clone-henna-pi.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
